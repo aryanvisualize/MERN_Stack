43e267ce02118274684d48c310b18ab650318bb6
aryanvisualize <aryanvisualize@gmail.com>
2026-08-30 03:10:44 +0530

    Add social_media_app

---
+++ SocialMedia_APP/Backend/src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const multer = require("multer");
+const uploadFile = require("./services/storage.service.js");
+const postModel = require("./models/post.model.js");
+const { image } = require('qr-image');
+
+app.use(express.json());
+
+const upload = multer({storage : multer.memoryStorage()});
+
+
+app.post('/create-post', upload.single("image"), async (req, res)=>{
+
+    const result = await uploadFile(req.file.buffer)
+
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+    return res.status(201).json({
+        message: "Post Created Successfully",
+        post
+    })
+})
+
+app.get('/posts', async (req, res)=>{
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message: "Post fetched successfully",
+        posts
+    })
+})
+
+module.exports = app;
---
+++ SocialMedia_APP/Backend/src/models/post.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    caption: {
+        type: String,
+        required: true
+    }
+});
+
+const postModel = mongoose.model("Post", postSchema);
+
+module.exports = postModel;
