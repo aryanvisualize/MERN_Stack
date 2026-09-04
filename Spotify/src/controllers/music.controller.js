8299d22024551197f6787ad5bd3d8cc78715cabb
aryanvisualize <aryanvisualize@gmail.com>
2026-09-04 14:25:05 +0530

    feat: add createAlbum functionality with JWT authorization and album model

--- Spotify/src/controllers/music.controller.js
+++ Spotify/src/controllers/music.controller.js
@@ -1,4 +1,7 @@
+const { requiredPaths } = require("../models/album.model.js");
 const musicModel = require("../models/music.model.js");
+const albumModel = require("../models/album.model.js");
+
 const { uploadFile } = require("../services/storage.service.js");
 const jwt = require("jsonwebtoken");
 
@@ -58,4 +61,40 @@ async function createMusic(req, res) {
   }
 }
 
-module.exports = { createMusic };
+async function createAlbum(req, res) {
+    const token = req.cookies.token;
+
+    if(!token) {
+      return res.status(401).json({message: "Unauthorized"})
+    }
+
+    try{
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if(decoded.role !== "artist"){
+          return res.status(403).json({message: "You dont have access to create album"});
+      }
+
+      const {title, musics} = req.body;
+      const album = await albumModel.create({
+        title,
+        artist: decoded.id,
+        musics: musics,
+      })
+      res.status(201).json({
+        message: "Album created successfully",
+        album: {
+            id: album._id,
+            title: album.title,
+            artist: album.artist,
+            musics: album.musics,
+        }
+      })
+
+    }
+    catch(err){
+      console.log(err);
+      return res.status(401).json({message: "Unauthorized"})
+    }
+}
+
+module.exports = { createMusic, createAlbum };
--- Spotify/src/routes/music.routes.js
+++ Spotify/src/routes/music.routes.js
@@ -9,7 +9,9 @@ const upload = multer({
 
 const router = express.Router();
 
-router.post("/upload", upload.single("music"), musicController.createMusic)
+router.post("/upload", upload.single("music"), musicController.createMusic);
+
+router.post("/album",musicController.createAlbum);
 
 
 module.exports = router;
