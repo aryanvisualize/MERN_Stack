9afc0051ae724f57fffcfb6cccc20b55166b4e65
aryanvisualize <aryanvisualize@gmail.com>
2026-09-04 18:59:42 +0530

    implemet role based authentication

--- Spotify/src/routes/music.routes.js
+++ Spotify/src/routes/music.routes.js
@@ -14,6 +14,6 @@ router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicC
 
 router.post("/album", authMiddleware.authArtist, musicController.createAlbum);
 
-router.get('/', musicController.getAllMusics)
+router.get('/', authMiddleware.authUser, musicController.getAllMusics)
 
 module.exports = router;
